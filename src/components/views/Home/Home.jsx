e07ae589e829146a20a6dbd735a52dce54b12589
Juan Manuel Gutierrez Monaco <juan.monaco.gutierrez@gmail.com>
2022-11-03 13:08:17 -0300

    Componente Header se elimino y se puso su contenido en Home

--- src/components/views/Home/Home.jsx
+++ src/components/views/Home/Home.jsx
@@ -1,5 +1,10 @@
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div className="App-header">
+            <img src={"./alkemy_logo.svg"} className="App-logo" alt="logo" />
+            <p>Bienvenido a AlkyBank</p>
+        </div>
+    );
 };
 
 export default Home;
