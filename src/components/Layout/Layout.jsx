eff5f0b922d355f62254960f2456bef77bd917c6
Lucas Cabral <lucascabral6008@gmail.com>
2022-11-03 18:01:58 -0300

    fixing routes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
@@ -8,18 +7,22 @@ import { NavBar } from "../NavBar/NavBar";
 import PrivateRoutes from "../../router/PrivateRoutes";
 import PublicRoutes from "../../router/PublicRoutes";
 
-
-
 const Layout = () => {
   // Componente para envolver a las rutas privadas
- 
+
   return (
     <>
       <BrowserRouter>
         <NavBar />
         <Header />
         <AnimatePresence>
-          {!localStorage.getItem("token") ? <PrivateRoutes /> : <PublicRoutes />}
+          {localStorage.getItem("token") ? (
+            <>
+              <PrivateRoutes /> <PublicRoutes />{" "}
+            </>
+          ) : (
+            <PublicRoutes />
+          )}
         </AnimatePresence>
       </BrowserRouter>
       <Footer />
