fa0baf26c05ad9c106cd0486bb3da08188bf97cd
Lucas Cabral <lucascabral6008@gmail.com>
2022-11-03 21:00:06 -0300

    fixing routing issues

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,32 +1,24 @@
-import { BrowserRouter } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
+import {BrowserRouter} from "react-router-dom";
+import {AnimatePresence} from "framer-motion";
 
 import Footer from "../Footer/Footer";
-import { NavBar } from "../NavBar/NavBar";
+import {NavBar} from "../NavBar/NavBar";
 import PrivateRoutes from "../router/PrivateRoutes";
 import PublicRoutes from "../router/PublicRoutes";
 
 const Layout = () => {
-    // Componente para envolver a las rutas privadas
-
-    return (
-        <>
-            <BrowserRouter>
-                <NavBar />
-                <AnimatePresence>
-                    {localStorage.getItem("token") ? (
-                        <>
-                            <PrivateRoutes />
-                            <PublicRoutes />
-                        </>
-                    ) : (
-                        <PublicRoutes />
-                    )}
-                </AnimatePresence>
-            </BrowserRouter>
-            <Footer />
-        </>
-    );
+  // Componente para envolver a las rutas privadas
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <AnimatePresence>
+          {localStorage.getItem("userData") ? <> <PrivateRoutes /></> : <PublicRoutes />}
+        </AnimatePresence>
+      </BrowserRouter>
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
--- src/components/router/PrivateRoutes.jsx
+++ src/components/router/PrivateRoutes.jsx
@@ -1,112 +1,172 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import {  motion } from "framer-motion";
-import Charge from '../views/Charge/Charge';
-import Spents from '../views/Spents/Spents';
-import Balance from '../views/Balance/Balance';
-import Movements from '../views/Movements/Movements';
-import Transfers from '../views/Transfers/Transfers';
-
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+import {motion} from "framer-motion";
+import Charge from "../views/Charge/Charge";
+import Spents from "../views/Spents/Spents";
+import Balance from "../views/Balance/Balance";
+import Movements from "../views/Movements/Movements";
+import Transfers from "../views/Transfers/Transfers";
+import Home from "../views/Home/Home";
+import { lazy, Suspense } from "react";
+import Register from "../views/auth/Register/Register";
+import Login from "../views/auth/Login/Login";
 
 const PrivateRoutes = () => {
+  const pageTransition = {
+    initial: {
+      opacity: 0,
+      x: "-100vw",
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        ease: [0.6, -0.05, 0.01, 0.99],
+      },
+    },
+    exit: {
+      x: "-100vw",
+      opacity: 0,
+      transition: {
+        duration: 1,
+        ease: [0.6, -0.05, 0.01, 0.99],
+      },
+    },
+  };
 
-    const pageTransition = {
-        initial: {
-          opacity: 0,
-          x: "-100vw",
-        },
-        animate: {
-          x: 0,
-          opacity: 1,
-          transition: {
-            duration: 1,
-            ease: [0.6, -0.05, 0.01, 0.99],
-          },
-        },
-        exit: {
-          x: "-100vw",
-          opacity: 0,
-          transition: {
-            duration: 1,
-            ease: [0.6, -0.05, 0.01, 0.99],
-          },
-        },
-      };
-    
-
+  const Error404 = lazy(() => import("../views/Error404/Error404"));
   return (
     <Routes>
-            <Route
-              path="/charge"
-              element={
-                <motion.div
-                  className="page"
-                  initial="initial"
-                  animate="animate"
-                  exit="exit"
-                  variants={pageTransition}>
-                  <Charge />
-                </motion.div>
-              }
-            />
+      <Route
+        path="/home"
+        element={
+          <motion.div
+            className="page"
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={pageTransition}>
+            <Home />
+          </motion.div>
+        }
+      />
 
-            <Route
-              path="/spents"
-              element={
-                <motion.div
-                  className="page"
-                  initial="initial"
-                  animate="animate"
-                  exit="exit"
-                  variants={pageTransition}>
-                  <Spents />
-                </motion.div>
-              }
-            />
+      <Route
+        path="/charge"
+        element={
+          <motion.div
+            className="page"
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={pageTransition}>
+            <Charge />
+          </motion.div>
+        }
+      />
 
-            <Route
-              path="/balance"
-              element={
-                <motion.div
-                  className="page"
-                  initial="initial"
-                  animate="animate"
-                  exit="exit"
-                  variants={pageTransition}>
-                  <Balance />
-                </motion.div>
-              }
-            />
+      <Route
+        path="/spents"
+        element={
+          <motion.div
+            className="page"
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={pageTransition}>
+            <Spents />
+          </motion.div>
+        }
+      />
 
-            <Route
-              path="/movements"
-              element={
-                <motion.div
-                  className="page"
-                  initial="initial"
-                  animate="animate"
-                  exit="exit"
-                  variants={pageTransition}>
-                  <Movements />
-                </motion.div>
-              }
-            />
+      <Route
+        path="/balance"
+        element={
+          <motion.div
+            className="page"
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={pageTransition}>
+            <Balance />
+          </motion.div>
+        }
+      />
 
-            <Route
-              path="/transfers"
-              element={
-                <motion.div
-                  className="page"
-                  initial="initial"
-                  animate="animate"
-                  exit="exit"
-                  variants={pageTransition}>
-                  <Transfers />
-                </motion.div>
-              }
-            />
+      <Route
+        path="/movements"
+        element={
+          <motion.div
+            className="page"
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={pageTransition}>
+            <Movements />
+          </motion.div>
+        }
+      />
+
+      <Route
+        path="/transfers"
+        element={
+          <motion.div
+            className="page"
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={pageTransition}>
+            <Transfers />
+          </motion.div>
+        }
+      />
+
+      <Route
+        path="/register"
+        element={
+          <motion.div
+            className="page"
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={pageTransition}>
+            <Register />
+          </motion.div>
+        }
+      />
+
+      <Route
+        path="/"
+        element={
+          <motion.div
+            className="page"
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={pageTransition}>
+            <Login />
+          </motion.div>
+        }
+      />
+
+      <Route
+        path="*"
+        element={
+          <motion.div
+            className="page"
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={pageTransition}>
+            <Suspense fallback={<>...</>}>
+              <Error404 />
+            </Suspense>
+          </motion.div>
+        }
+      />
     </Routes>
-  )
-}
+  );
+};
 
-export default PrivateRoutes
+export default PrivateRoutes;
