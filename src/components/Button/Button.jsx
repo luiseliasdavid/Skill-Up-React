815b15dc56728f7965e68f0e8e6b0c953a8183a3
Facundo Alvarez <alvarezfacundo321@gmail.com>
2022-11-04 16:12:48 -0300

    Changes in Nav, Footer, Title and Button
    -Add PropTypes and new props in Button + title
    -Nav conditional when the user is login
    -Footer in top + Styles in css

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-
-export const Button = ({ variant = "correct", action, text }) => {
+import PropTypes from "prop-types";
+export const Button = ({ variant = "primary", action, text = "Sin texto" }) => {
   return (
-    <button onClick={action} className={`${variant}`}>
+    <button onClick={action} className={`btn-${variant}`}>
       {text}
     </button>
   );
 };
+
+Button.propTypes = {
+  variant: PropTypes.string,
+  text: PropTypes.string.isRequired,
+  action: PropTypes.func,
+};
+
 export default Button;
