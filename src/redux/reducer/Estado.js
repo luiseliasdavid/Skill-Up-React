1011861abe8de1198606b7fd927b34f7db8c6563
tobiaszavallo <tobi-zavallo@hotmail.com>
2022-11-06 22:29:28 -0300

    nnn

--- src/redux/reducer/Estado.js
+++ src/redux/reducer/Estado.js
@@ -1,81 +1,82 @@
-import React, { useEffect } from "react";
-import {
-   createUser,
-   userList,
-   login,
-   logout,
-   createAccount,
-   addMoneyToAccount,
-   balance,
-} from "../actions";
-import { useDispatch, useSelector } from "react-redux";
+import React,{useEffect} from 'react'
+import {createUser , userList , login ,logout, createAccount, 
+  addMoneyToAccount, balance } from "../actions"
+import { useDispatch, useSelector } from 'react-redux';
 
-export const Estado = () => {
-   const API_SWAGGER =
-      "http://wallet-main.eba-ccwdurgr.us-east-1.elasticbeanstalk.com";
 
-   let data = useSelector((state) => state);
+export const Estado = ()=> {
 
-   const dispatch = useDispatch();
+// El 'useEffect' en el componente Login era el que borraba el token
+// del localStorage. 
 
-   useEffect(() => {
-      if (data) {
-         console.log(data);
-      }
-      //result 11-3-2022   // necesitamos esto -> "2022-10-26 10:00:00"
-   }, [data]);
+// Ahora queda solucionar que al recargar la pagina no se pierdan los datos
+// que tenemos en el store.
 
-   const crearUsuario = () => {
-      console.log("hoola");
-      dispatch(
-         createUser({
-            first_name: "holsa1asd",
-            last_name: "as1shola",
-            email: "hola1sssdasd@gmail.com",
-            password: "123456",
-            points: 0,
-            roleId: 2,
-         })
-      );
-   };
-   const log = () => {
-      dispatch(
-         login({
-            email: "hola1ssdasd@gmail.com",
-            password: "123456",
-         })
-      );
-   };
 
-   const cargasaldo = () => {
-      dispatch(addMoneyToAccount(740, data.userData.account.id));
-   };
+let data = useSelector(state=> state)
 
-   const balanceview = () => {
-      dispatch(balance());
-   };
+const dispatch = useDispatch()
 
-   const userLogout = () => {
-      dispatch(logout());
-   };
-   const consoleEstate = () => {
-      console.log(data);
-      console.log(JSON.parse(localStorage.getItem("user")));
-      if (data.userdata) {
-         console.log(JSON.parse(localStorage.getItem("user")).id);
-      }
-   };
+useEffect(()=>{
+ if(data){
+ console.log(data)}
+//result 11-3-2022   // necesitamos esto -> "2022-10-26 10:00:00"
+},[data])
 
-   const crearCuenta = () => {
-      dispatch(createAccount(67));
-   };
-   return (
-      <div>
-         <button onClick={consoleEstate}>Console.log estado</button>
-         <button onClick={crearUsuario}>cear usuario </button>
-         <button onClick={log}>login</button>
-         <button onClick={cargasaldo}>CARGAR SALDO</button>
-         <button onClick={balanceview}>BALANCE</button>
-      </div>
-   );
-};
+const crearUsuario= ()=>{
+  console.log("hoola")
+ dispatch( createUser({
+    
+    first_name: "lioffpsp",
+    last_name: "oodredss",
+    email: "ekkkxcz@ail.com",
+    password: "123456",
+    points: 0,
+    roleId: 2,
+    
+  }))
+}
+const log=()=>{
+ dispatch(login({
+  email: "ekkkxcz@ail.com",
+  password: "123456"
+}))
+}
+
+const cargasaldo = () => {
+  dispatch( addMoneyToAccount( 740 ,data.userData.account.id  ) );
+}
+
+const balanceview = () => {
+  dispatch(balance())
+}
+
+
+
+const userLogout = () => {
+  dispatch(logout());
+}
+const consoleEstate = ()=>{
+  console.log(data)
+  console.log(JSON.parse(localStorage.getItem("user")))
+  if(data.userdata){
+  console.log(JSON.parse(localStorage.getItem("user")).id)}
+}
+
+/* const crearCuenta =()=> {
+  dispatch(createAccount(67))
+} */
+  return (
+    <div>
+      <button onClick={consoleEstate}>Console.log estado</button>
+      <button onClick={crearUsuario}>crear usuario </button>
+      <button onClick={log}>login</button>
+      <button onClick={cargasaldo}>CARGAR SALDO</button>
+      <button onClick={balanceview}>BALANCE</button>
+      <button onClick={userLogout}>LOGOUT</button>
+      
+    </div>
+  )
+}
+
+
