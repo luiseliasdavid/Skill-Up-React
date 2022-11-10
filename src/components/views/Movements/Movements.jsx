a7c364ed4305375eaaf435605365153e342db37c
tobiaszavallo <tobi-zavallo@hotmail.com>
2022-11-09 21:02:27 -0300

    getMovements ready

--- src/components/views/Movements/Movements.jsx
+++ src/components/views/Movements/Movements.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { getAllMovements } from '../../../redux/actions';
+
 
 const Movements = () => {
+
+
+  const dispatch = useDispatch();
+
+
+  const getMovements = (e) => {
+    dispatch(getAllMovements(e.target.value))   
+  }
+
   return (
-    <div>Movements</div>
+    <div>
+      <h1>Movements</h1>
+      <button onClick={getMovements} value={1}>1</button>
+      <button onClick={getMovements} value={2}>2</button>
+      <button onClick={getMovements} value={3}>3</button>
+      <button onClick={getMovements} value={4}>4</button>
+    </div>
   )
 }
 
