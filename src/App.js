4aaba9fa3c8bfecfe4a56c68af6c366cd18cc306
Juan Manuel Gutierrez Monaco <104147035+juangutierrezmonaco@users.noreply.github.com>
2022-11-11 17:08:07 -0300

    Gastos added (#10)
    * Component Spents Added
    * Redux incorporated to Register Component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.min.css';
 import Layout from './components/Layout/Layout';
 import { Estado } from './redux/reducer/Estado';
 
-function App() {
+function App() {    
     return (
         <div className="App">
             <Layout />
