b69895d7715413664e2c7137a10469d4683c628f
Juan Manuel Gutierrez Monaco <juan.monaco.gutierrez@gmail.com>
2022-11-11 02:34:51 -0300

    Component Navbar reviewed

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 export const Button = ({ variant = "primary", action, text = "Sin texto" }) => {
-  return (
-    <button onClick={action} className={`btn-${variant}`}>
-      {text}
-    </button>
-  );
+    return (
+        <button onClick={action} className={`btn-${variant}`}>
+            {text}
+        </button>
+    );
 };
 
 Button.propTypes = {
-  variant: PropTypes.string,
-  text: PropTypes.string.isRequired,
-  action: PropTypes.func,
+    variant: PropTypes.string,
+    text: PropTypes.string.isRequired,
+    action: PropTypes.func,
 };
 
 export default Button;
--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -12,154 +12,157 @@ import {
     GET_ALL_USERS_WITH_ACCOUNT,
     CLEAN_STATUS_REQUEST,
     /* CLEAN_STORE, */
- } from "../actions";
- 
- const initialState = {
-    statusRequest: { status:'0' },
+} from "../actions";
+
+const initialState = {
+    statusRequest: { status: '0' },
     userData: {},
     userList: [],
     movements: [],
- };
- 
- const rootReducer = (state = initialState, action) => {
+    isLogged: false,
+};
+
+const rootReducer = (state = initialState, action) => {
     switch (action.type) {
-       case POST_NEW_USER:
-          return {
-             ...state,
-             statusRequest: action.payload,
-          };
-       case POST_ACCOUNT:
-          return {
-             ...state,
-             statusRequest: action.status,
-             userData: {
-                ...action.payload.user,
-                account: action.payload.account,
-             },
-          };
-       case GET_USER_DATA:
-          if (action.status.status === 200) {
-             return {
+        case POST_NEW_USER:
+            return {
+                ...state,
+                statusRequest: action.payload,
+            };
+        case POST_ACCOUNT:
+            return {
                 ...state,
                 statusRequest: action.status,
                 userData: {
-                   ...state.userData,
-                   ...action.payload.user,
-                   account: action.payload.account,
+                    ...action.payload.user,
+                    account: action.payload.account,
                 },
-             };
-          }else {
-             return {
-                ...state,
-                statusRequest: action.status
-             }
-          }
-       case LOGIN:
-          return {
-             ...state,
-             statusRequest: action.status,
-            /*  userData: {
-                ...action.payload.user,
-                account: action.payload.account,
-             }, */
-          };
-       case LOGOUT:
-          return {
-             ...state,
-             userData: {},
-             userList: [],
-             movements: [],
-             statusRequest: { status: '0' }
-          };
-       case CLEAN_STATUS_REQUEST:
-          return {
-             ...state,
-             statusRequest: { status: '0' }
-          }
-       /* case CLEAN_STORE:
-          return {
-             ...state,
-             userData: {},
-             movements: [],
-             statusRequest: { status: '0' }
-          } */
-       case POST_ADD_CASH:
-          if( action.status.status === 200 ) {
-             return {
+            };
+        case GET_USER_DATA:
+            if (action.status.status === 200) {
+                return {
+                    ...state,
+                    statusRequest: action.status,
+                    userData: {
+                        ...state.userData,
+                        ...action.payload.user,
+                        account: action.payload.account,
+                    },
+                };
+            } else {
+                return {
+                    ...state,
+                    statusRequest: action.status
+                }
+            }
+        case LOGIN:
+            return {
                 ...state,
                 statusRequest: action.status,
-                userData: { ...state.userData, account: action.payload },
-             };
-          }else {
-             return {
+                isLogged: true,
+                /*  userData: {
+                    ...action.payload.user,
+                    account: action.payload.account,
+                 }, */
+            };
+        case LOGOUT:
+            return {
                 ...state,
-                statusRequest: action.status
-             };
-          };
-       case SEND_MONEY:
-          if (action.status.status === 200) {
-             return {
+                userData: {},
+                userList: [],
+                movements: [],
+                statusRequest: { status: '0' },
+                isLogged: false,
+            };
+        case CLEAN_STATUS_REQUEST:
+            return {
                 ...state,
-                statusRequest: action.status,
-                userData: {
-                   ...state.userData,
-                   ...action.payload.user,
-                   account: action.payload.account,
-                },
-             };
-          }else {
-             return {
-                ...state,
-                statusRequest: action.status
-             }
-          }
-       case GET_BALANCE:
-          if( action.status.status === 200 ) {
-             return {
-                ...state,
-                userData: {
-                   ...state.userData,
-                   balance: action.payload,
-                   transactions: {
-                      topup: action.topupList,
-                      payments: action.paymentsList,
-                   },
-                },
-             };
-          }else {
-             return {
-                ...state,
-                statusRequest: action.status
-             };
-          };
-       case GET_ALL_MOVEMENTS:
-          if( action.status.status === 200 ) {
-             return {
-                ...state,
-                statusRequest: action.status,
-                movements: action.payload
-             };
-          } else {
-             return {
-                ...state,
-                statusRequest: action.status
-             };
-          };
-       case GET_ALL_USERS_WITH_ACCOUNT:
-          if( action.status.status === 200 ) {
-             return {
-                ...state,
-                userList: action.payload,
-             };
-          }else {
-             return {
-                ...state,
-                statusRequest: action.status
-             };
-          };
-       default:
-          return state;
+                statusRequest: { status: '0' }
+            }
+        /* case CLEAN_STORE:
+           return {
+              ...state,
+              userData: {},
+              movements: [],
+              statusRequest: { status: '0' }
+           } */
+        case POST_ADD_CASH:
+            if (action.status.status === 200) {
+                return {
+                    ...state,
+                    statusRequest: action.status,
+                    userData: { ...state.userData, account: action.payload },
+                };
+            } else {
+                return {
+                    ...state,
+                    statusRequest: action.status
+                };
+            };
+        case SEND_MONEY:
+            if (action.status.status === 200) {
+                return {
+                    ...state,
+                    statusRequest: action.status,
+                    userData: {
+                        ...state.userData,
+                        ...action.payload.user,
+                        account: action.payload.account,
+                    },
+                };
+            } else {
+                return {
+                    ...state,
+                    statusRequest: action.status
+                }
+            }
+        case GET_BALANCE:
+            if (action.status.status === 200) {
+                return {
+                    ...state,
+                    userData: {
+                        ...state.userData,
+                        balance: action.payload,
+                        transactions: {
+                            topup: action.topupList,
+                            payments: action.paymentsList,
+                        },
+                    },
+                };
+            } else {
+                return {
+                    ...state,
+                    statusRequest: action.status
+                };
+            };
+        case GET_ALL_MOVEMENTS:
+            if (action.status.status === 200) {
+                return {
+                    ...state,
+                    statusRequest: action.status,
+                    movements: action.payload
+                };
+            } else {
+                return {
+                    ...state,
+                    statusRequest: action.status
+                };
+            };
+        case GET_ALL_USERS_WITH_ACCOUNT:
+            if (action.status.status === 200) {
+                return {
+                    ...state,
+                    userList: action.payload,
+                };
+            } else {
+                return {
+                    ...state,
+                    statusRequest: action.status
+                };
+            };
+        default:
+            return state;
     }
- };
- 
- export default rootReducer;
+};
+
+export default rootReducer;
