911e27e735bc4c1eacfdbbdd91312a294e6ff4db
Juan Manuel Gutierrez Monaco <104147035+juangutierrezmonaco@users.noreply.github.com>
2022-11-11 15:12:52 -0300

    Revision of some components (#9)
    * Component Navbar reviewed
    
    * Component Button reviewed
    
    * Component Title reviewed
    
    * Layout Component reviewed

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,17 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types";
-export const Button = ({ variant = "primary", action, text = "Sin texto" }) => {
-  return (
-    <button onClick={action} className={`btn-${variant}`}>
-      {text}
-    </button>
-  );
-};
-
-Button.propTypes = {
-  variant: PropTypes.string,
-  text: PropTypes.string.isRequired,
-  action: PropTypes.func,
+export const Button = ({ variant = "primary", action, text = "" }) => {
+    return (
+        <button onClick={action} className={`btn btn-${variant}`}>
+            {text}
+        </button>
+    );
 };
 
 export default Button;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,111 +1,97 @@
-import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { logout } from "../../redux/actions";
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import Title from "../Title/Title";
-export const NavBar = () => {
-  const dispatch = useDispatch();
 
-  const [isLogin, setIsLogin] = useState(false);
-  const [firstName, setFirstName] = useState(null);
+import { logout } from "../../redux/actions";
 
-  const navigate = useNavigate();
+const Navbar = () => {
+    const dispatch = useDispatch();
 
-  let userData = localStorage.getItem("userData");
+    const isLogged = useSelector((state) => state.isLogged);
+    const firstName = useSelector((state) => state.userData?.first_name);
 
-  useEffect(() => {
-    if (!!userData) {
-      setIsLogin(true);
-      setFirstName(JSON.parse(userData).first_name);
-    }
-  }, []);
+    const handleLogout = () => {
+        dispatch(logout());
+    };
 
-  const handleLogOut = () => {
-    dispatch(logout()).then((res) => {
-      setIsLogin(true);
-      navigate("/", { redirect: true });
-    });
-  };
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid ">
+                    <div id="nav-left-container">
+                        <Link to="/" className="nav-link">
+                            <img
+                                src="/assets/img/logo.png"
+                                alt="brand-logo"
+                                /* Cambiar tamaños */ width="30"
+                                /* Cambiar tamaños */ height="24"
+                            />
+                            <span className="ms-2 text-capitalize nav-item">
+                                {isLogged && firstName
+                                    ? `¡Bienvenido ${firstName}!`
+                                    : "¡Bienvenido!"}
+                            </span>
+                        </Link>
+                    </div>
 
-  return (
-    <header>
-      <nav className="navbar navbar-expand-lg bg-light">
-        <div className="container-fluid ">
-          <div id="nav-left-container">
-            <Link to="/" className="nav-link">
-              <img
-                src={require("./assets/logo.png")}
-                alt="brand-logo"
-                /* Cambiar tamaños */ width="30"
-                /* Cambiar tamaños */ height="24"
-              />
-              {!isLogin ? (
-                <span className="ms-2 text-capitalize nav-item">
-                  ¡Bienvenido!
-                </span>
-              ) : (
-                <span className="ms-2 text-capitalize nav-item">
-                  {firstName}
-                </span>
-              )}
-            </Link>
-          </div>
-
-          <div
-            id="nav-right-container"
-            className="collapse navbar-collapse d-flex justify-content-end"
-          >
-            {!isLogin ? (
-              <div>
-                <Link to="/">
-                  <button className="btn btn-primary">Login</button>
-                </Link>
-                <Link to="/register">
-                  <button className="btn btn-warning ms-3">Register</button>
-                </Link>
-              </div>
-            ) : (
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <Link to="/charge" className="nav-link">
-                    Carga de saldo
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/spents" className="nav-link">
-                    Gastos
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/balance" className="nav-link">
-                    Balance
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/movements" className="nav-link">
-                    Movimientos
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/transfers" className="nav-link">
-                    Envio de dinero
-                  </Link>
-                </li>
-                <Link to="/">
-                  <button
-                    onClick={() => handleLogOut()}
-                    className="btn btn-danger"
-                  >
-                    LogOut
-                  </button>
-                </Link>
-              </ul>
-            )}
-          </div>
-        </div>
-      </nav>
-    </header>
-  );
+                    <div
+                        id="nav-right-container"
+                        className="collapse navbar-collapse d-flex justify-content-end"
+                    >
+                        {!isLogged ? (
+                            <div>
+                                <Link to="/login">
+                                    <button className="btn btn-primary">
+                                        Login
+                                    </button>
+                                </Link>
+                                <Link to="/register">
+                                    <button className="btn btn-warning ms-3">
+                                        Register
+                                    </button>
+                                </Link>
+                            </div>
+                        ) : (
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <Link to="/charge" className="nav-link">
+                                        Carga de saldo
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/spents" className="nav-link">
+                                        Gastos
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/balance" className="nav-link">
+                                        Balance
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/movements" className="nav-link">
+                                        Movimientos
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/transfers" className="nav-link">
+                                        Envio de dinero
+                                    </Link>
+                                </li>
+                                <Link to="/">
+                                    <button
+                                        onClick={handleLogout}
+                                        className="btn btn-danger"
+                                    >
+                                        <i className="fa-solid fa-right-from-bracket"></i>
+                                    </button>
+                                </Link>
+                            </ul>
+                        )}
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
 };
+
+export default Navbar;
