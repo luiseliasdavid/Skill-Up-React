1c47ac736132b75e60fef359d08a8704ec3eb782
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-11-10 21:22:56 -0300

    init balance view

--- src/components/views/Balance/Balance.jsx
+++ src/components/views/Balance/Balance.jsx
@@ -1,50 +1,43 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { balance, cleanStatusRequest, userData } from '../../../redux/actions';
-
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { balance, cleanStatusRequest, userData } from "../../../redux/actions";
+import balanceStyles from "./balanceStyles.scss";
 
 const Balance = () => {
+   let data = useSelector((state) => state.userData);
+   let request = useSelector((state) => state.statusRequest);
 
-  let data = useSelector((state) => state.userData);
-  let request = useSelector( (state) => state.statusRequest );
+   const dispatch = useDispatch();
+   const navigate = useNavigate();
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+   const token = localStorage.getItem("token");
 
+   useEffect(() => {
+      if (token !== null) {
+         dispatch(userData());
+         dispatch(balance());
+      } else {
+         navigate("/login");
+      }
+   }, [dispatch, token, navigate]);
 
-  const token = localStorage.getItem("token");
+   useEffect(() => {
+      if (request.status === 200) {
+         dispatch(cleanStatusRequest());
+      }
+      if (request.status === "0") return;
+      if (request.status !== 200) {
+         alert(`error al buscar el balance`);
+         dispatch(cleanStatusRequest());
+      }
+   }, [dispatch, request]);
 
-    useEffect(() => {
-        if ( token !== null ) {
-            dispatch( userData() );
-            dispatch(balance());
-        } else {
-            navigate('/login')
-        }
-    }, [dispatch, token, navigate ])
-    
+   return (
+      <div className="container">
+         <h1>balance</h1>
+      </div>
+   );
+};
 
-
-    useEffect(() => {
-        if ( request.status === 200 ) {
-            dispatch(cleanStatusRequest());
-        }
-        if ( request.status === '0' ) return;
-        if ( request.status !== 200 ) {
-           alert(`error al buscar el balance`)
-           dispatch(cleanStatusRequest());
-        }   
-     }, [ dispatch, request ])  
-
-
-
-  return (
-    <div>
-      <h1>balance</h1>
-    </div>
-  )
-}
-
-export default Balance
+export default Balance;
