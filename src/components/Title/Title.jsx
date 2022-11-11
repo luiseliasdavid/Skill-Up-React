5da945c4238bdcf9c88e6f0510aae1731fb6c2f3
Juan Manuel Gutierrez Monaco <juan.monaco.gutierrez@gmail.com>
2022-11-11 02:50:18 -0300

    Component Title reviewed

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,16 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types";
-
-export const Title = ({ type = "h1", text, className }) => {
-  const title = `<${type} className='${className}'>${text}</${type}>`;
-
-  return <div dangerouslySetInnerHTML={{ __html: title }}></div>;
-};
-
-Title.propTypes = {
-  type: PropTypes.string,
-  text: PropTypes.string.isRequired,
-  className: PropTypes.string,
+const Title = ({ Type = "h1", text, className }) => {
+    return (
+        <Type className={className}>
+            {text}
+        </Type>
+    );
 };
 
 export default Title;
