c0febb19c4c5030101127784a955568a88a68572
Juan Manuel Gutierrez Monaco <104147035+juangutierrezmonaco@users.noreply.github.com>
2022-11-11 00:37:22 -0300

    Transfers logic added and redux actions working fine (#7)
    * transfers form added
    
    * redux actions updated
    
    author: luiseliasdavid <luis.elias.david@gmail.com>

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -150,24 +150,24 @@ export const login = (user) => {
          };
          localStorage.setItem("user", JSON.stringify(userDataStorage));
 
-         /* const transactionsUser = await fetchWalletApi.get(`/transactions`);
+         const transactionsUser = await fetchWalletApi.get(`/transactions`);
          
          const initialTopup = transactionsUser.data.data[0];
          
          console.log(initialTopup);
          const idAccount = initialTopup.accountId;
-         const account = await fetchWalletApi.get(`/accounts/${idAccount}`); */
+         const account = await fetchWalletApi.get(`/accounts/${idAccount}`);
 
          return dispatch({
             type: LOGIN,
-            /* payload: {  user: info.data, account: account.data }, */
+            payload: {  user: info.data, account: account.data },
             status: { status: 200, message:'OK' }
          });
       } catch (e) {
          console.log(e);
          return dispatch({
             type: LOGIN,
-           /*  payload: { user: {}, account: {} }, */
+            payload: { user: {}, account: {} },
             status: { status: e.response.data.status, message: e.response.data.error }
          });
       }
@@ -394,16 +394,32 @@ export const getAllUsersWithAccount = () => {
 
 
 export const sendMoneyToUser = (destinyAccountId,amountToSend,concept,moneyInMyAccount,idOfMyAccont, idMyUser ) => {
+   
    return async function (dispatch) {
-      try {
+       if(amountToSend > moneyInMyAccount) {
+         return dispatch({
+            type: SEND_MONEY,
+            status: { status: 500, message: 'Fondos insuficientes' },
+         });
+       }  
+         try{
          let paymentBody= {
             type: "payment",
             concept: concept,
             amount: amountToSend
           }
           
-          await fetchWalletApi.post(`/accounts/${destinyAccountId}`,paymentBody);
-    
+          let response= await fetchWalletApi.post(`/accounts/${destinyAccountId}`,paymentBody);
+          
+         }
+          catch(e){
+            
+            return dispatch({
+               type: SEND_MONEY,
+               status: { status: e.response.data.status, message: e.response.data.error },
+            });
+          }
+          try {
           let NewAmount = Number( moneyInMyAccount) - Number(amountToSend)
           
           let putNewAmount = {
@@ -413,7 +429,7 @@ export const sendMoneyToUser = (destinyAccountId,amountToSend,concept,moneyInMyA
              userId: idMyUser
            }
            
-         await fetchWalletApi.put(`/accounts/${idOfMyAccont}`, putNewAmount);       
+         let responsePut = await fetchWalletApi.put(`/accounts/${idOfMyAccont}`, putNewAmount);       
            
          const userDetail = await fetchWalletApi.get(`/auth/me`);
    
@@ -423,7 +439,7 @@ export const sendMoneyToUser = (destinyAccountId,amountToSend,concept,moneyInMyA
    
          const idAccount = initialTopup.accountId;
          const account = await fetchWalletApi.get(`/accounts/${idAccount}`);
-
+          console.log(responsePut)
          return dispatch({
             type: SEND_MONEY,
             payload: { user: userDetail.data, account: account.data },
@@ -431,7 +447,7 @@ export const sendMoneyToUser = (destinyAccountId,amountToSend,concept,moneyInMyA
          });
 
       } catch(e) {
-         console.log(e);
+         
          return dispatch({
             type: SEND_MONEY,
             status: { status: e.response.data.status, message: e.response.data.error },
