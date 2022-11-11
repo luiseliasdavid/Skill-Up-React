6e71d51d9f14415cf3fef03da90afe5b46f102f5
Daiana Gimenez <87146334+NamineD@users.noreply.github.com>
2022-11-11 16:23:41 -0300

    Add Component Spents

---
+++ Register/Register.jsx
@@ -0,0 +1,197 @@
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import Swal from "../../../../utils/swal";
+import toast from "../../../../utils/toast";
+import "./Register.css";
+
+const Register = () => {
+  const navigate = useNavigate();
+
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    roleId: 1,
+  };
+
+  const walletApi = axios.create({
+    baseURL: "http://wallet-main.eba-ccwdurgr.us-east-1.elasticbeanstalk.com",
+  });
+
+  const onSubmit = () => {
+    walletApi
+      .post("/users", {
+        first_name: values.firstName,
+        last_name: values.lastName,
+        email: values.email,
+        password: values.password,
+        roleId: 1,
+      })
+      .then((res) => res.data)
+      .then((res) => {
+        // Falta lógica para que se avise el status de la response. Si está duplicado se debe avisar también.
+        console.log("Form OK");
+        localStorage.setItem("userData", JSON.stringify(res));
+        toast("Registro con exito", "success");
+        navigate("/", { replace: true });
+      })
+      .catch((error) => {
+        //console.log(error.response);
+        if (error.response.data.status === 500) {
+          toast("El email ya se encuentra en uso", "warning");
+        }  
+      });
+  };
+
+  const required = "* Campo obligatorio.";
+
+  const validationSchema = Yup.object().shape({
+    showEmail: Yup.boolean(),
+    firstName: Yup.string().required(required),
+    lastName: Yup.string().required(required),
+    email: Yup.string().email("Debe ser un email válido.").required(required),
+    password: Yup.string().required(required),
+  });
+
+  // useFormik espera los parámetros initialValues, validationSchema y onSubmit.
+  const formik = useFormik({ initialValues, validationSchema, onSubmit });
+
+  const { errors, touched, values, handleChange, handleBlur, handleSubmit } =
+    formik;
+
+  return (
+    <div className="d-flex justify-content-center row">
+      {/* El que se encarga de hacer el submit es el handleSubmit */}
+      <form
+        onSubmit={handleSubmit}
+        className="col-10 d-flex flex-column align-items-center g-3"
+      >
+        <h1>Creá tu cuenta</h1>
+
+        <label
+          htmlFor="inputFirstName"
+          className="col-6 d-flex flex-column mb-3"
+        >
+          <span className="label-color form-label">Nombre</span>
+          <input
+            type="text"
+            name="firstName"
+            autoComplete="off"
+            value={values.firstName}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={
+              errors.firstName && touched.firstName
+                ? "form-control is-invalid"
+                : "form-control"
+            }
+          />
+          {errors.firstName && touched.firstName && (
+            <div
+              id="validationServerUsernameFeedback"
+              className="invalid-feedback"
+            >
+              {errors.firstName}
+            </div>
+          )}
+        </label>
+
+        <label
+          htmlFor="inputLastName"
+          className="col-6 d-flex flex-column mb-3"
+        >
+          <span className="label-color form-label">Apellido</span>
+          <input
+            type="text"
+            name="lastName"
+            autoComplete="off"
+            value={values.lastName}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={
+              errors.lastName && touched.lastName
+                ? "form-control is-invalid"
+                : "form-control"
+            }
+          />
+          {errors.lastName && touched.lastName && (
+            <div
+              id="validationServerUsernameFeedback"
+              className="invalid-feedback"
+            >
+              {errors.lastName}
+            </div>
+          )}
+        </label>
+
+        <label htmlFor="inputEmail" className="col-6 d-flex flex-column mb-3">
+          <span className="label-color form-label">Email</span>
+          <input
+            type="email"
+            name="email"
+            autoComplete="off"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={
+              errors.email && touched.email
+                ? "form-control is-invalid"
+                : "form-control"
+            }
+          />
+          {errors.email && touched.email && (
+            <div
+              id="validationServerUsernameFeedback"
+              className="invalid-feedback"
+            >
+              {errors.email}
+            </div>
+          )}
+        </label>
+
+        <label
+          htmlFor="inputPassword"
+          className="col-6 d-flex flex-column mb-3"
+        >
+          <span className="label-color form-label">Contraseña</span>
+          <input
+            type="password"
+            name="password"
+            autoComplete="off"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={
+              errors.password && touched.password
+                ? "form-control is-invalid"
+                : "form-control"
+            }
+          />
+          {errors.password && touched.password && (
+            <div
+              id="validationServerUsernameFeedback"
+              className="invalid-feedback"
+            >
+              {errors.password}
+            </div>
+          )}
+        </label>
+
+        <div className="col-sm-12">
+          <button type="submit" className="btn btn-outline-primary">
+            Registrarse
+          </button>
+        </div>
+
+        <div className="mt-3">
+          <Link to="/">Iniciar Sesión</Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
