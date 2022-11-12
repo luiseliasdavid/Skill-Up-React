18c7c2016cb492652fef24dd60292af8ad3d183d
Lucas Cabral <lucascabral6008@gmail.com>
2022-11-12 11:55:12 -0300

    adding responsive

--- src/components/views/Home/Home.jsx
+++ src/components/views/Home/Home.jsx
@@ -60,10 +60,10 @@ const Home = () => {
   ]);
 
   return (
-    <div className={`container mt-5 ${styles.bgColor}`}>
+    <div className={`container-xl mt-5 ${styles.bgColor} container-fluid-lg`}>
       <div className="row vh-100">
-        <div className="col-4 d-flex flex-column align-items-center justify-content-evenly ">
-          <div className={`card w-75 ${styles.cardHeightLeft} `}>
+        <div className="col-12 col-md-4  d-md-flex d-flex flex-column align-items-center justify-content-md-evenly justify-content-start pt-5 ">
+          <div className={`card w-75  ${styles.cardHeightLeft} `}>
             <p className="mt-2">Available money</p>
             <h2 className="mt-2">
               {hide ? currencyFormatter(value) : "$ ******"}
@@ -74,23 +74,23 @@ const Home = () => {
           </div>
 
           <div
-            className={`card w-75 d-flex flex-column justify-content-center ${styles.cardHeightLeft}`}>
+            className={`card w-75  d-flex flex-column justify-content-center d-none d-md-flex  ${styles.cardHeightLeft}`}>
             <span>Recomendá la App y ganá!</span>
             <hr />
             <span>Recargá crédito en tu SUBE y tu celular</span>
           </div>
 
           <div
-            className={`card w-75 p-3  d-flex justify-content-center ${styles.cardHeightLeft}`}>
-            <img src={"./alkemy_logo.svg"} alt="" />
+            className={`card w-75 p-3 d-flex justify-content-center d-none d-md-flex ${styles.cardHeightLeft}`}>
+            <img src={"./assets/img/alkemy_logo.svg"} alt="" />
           </div>
           <div
-            className={`card w-75 ${styles.regret} d-flex justify-content-center margin-auto flex-row  align-items-center`}>
+            className={`card w-75 ${styles.regret} d-flex justify-content-center margin-auto flex-row d-none d-md-flex  align-items-center`}>
             <span className={`styles.`}>Regret button</span>
           </div>
-        </div>
-        <div className="col-8 text-center  d-flex justify-content-center align-items-center flex-column">
-          <div className={`card w-75  ${styles.cardHeight}`}>
+        </div> 
+        <div className="col-12 col-md-8 text-center d-flex justify-content-center align-items-center flex-column">
+          <div className={`card w-75  ${styles.cardHeight} position-relative overflow-scroll`}>
             <h5 className="bg-info ">Last movements</h5>
 
             {!loading ? (
