9d425df89e7a86c29c3225a2427669f62a01ca53
Daiana Gimenez <87146334+NamineD@users.noreply.github.com>
2022-11-12 18:51:45 -0300

    Funcionalidad Spent terminada

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,7 +3,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import { lazy, Suspense } from "react";
 
 import PrivateRoutes from "../router/PrivateRoutes";
-import Navbar from "../Navbar/NavBar";
+import Navbar from "../NavBar/NavBar"
 import Footer from "../Footer/Footer";
 import Register from "../views/auth/Register/Register";
 import Login from "../views/auth/Login/Login";
